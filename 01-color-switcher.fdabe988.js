e9f0aaa44b6689e15a2acf91ba5a2b07f4d36f23
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2022-11-30 15:13:29 +0000

    Deploying to gh-pages from @ SergeyMaitamal7/goit-js-hw-09@e5d336fb94f96c5e5ca7f0c8b308a6d58809913f 🚀

--- 01-color-switcher.fdabe988.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.setAttribute("disabled",!0);let a=null;e.addEventListener("click",(()=>{a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),d.disabled=!1,e.disabled=!0})),d.addEventListener("click",(()=>{clearInterval(a),d.disabled=!0,e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.fdabe988.js.map
